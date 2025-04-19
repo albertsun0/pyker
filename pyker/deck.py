594f66a44bb9cd27f35f143429beadedb2bce869
Joshua Goon <j.j.jappiecraft@gmail.com>
2025-04-18 23:07:43 -0700

    feat: scaffold deck

---
+++ pyker/deck.py
@@ -0,0 +1,21 @@
+import random
+from pyker.card import Card, Rank, Suit
+
+
+class Deck:
+    def __init__(self):
+        self.cards = self._create_deck()
+
+    def _create_deck(self):
+        return [Card(suit, rank) for suit in Suit for rank in Rank]
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self):
+        if len(self.cards) == 0:
+            return None
+        return self.cards.pop()
+
+    def __str__(self):
+        return " ".join(str(card) for card in self.cards)
