cbae53f1eadd02aaf95bf9531cbc89cff7906f3c
Joshua Goon <j.j.jappiecraft@gmail.com>
2025-04-18 23:04:18 -0700

    feat: implement `__str__` interface

--- pyker/card.py
+++ pyker/card.py
@@ -1,11 +1,38 @@
 from enum import Enum, auto
 
+SUIT_DISPLAY_TEXT = {
+    "CLUB": "♣",
+    "SPADE": "♠",
+    "HEART": "♥",
+    "DIAMOND": "♦",
+}
+
 
 class Suit(Enum):
     CLUB = auto()
     DIAMOND = auto()
     HEART = auto()
     SPADE = auto()
+
+    def __str__(self):
+        return SUIT_DISPLAY_TEXT[self.name]
+
+
+RANK_DISPLAY_TEXT = {
+    "TWO": "2",
+    "THREE": "3",
+    "FOUR": "4",
+    "FIVE": "5",
+    "SIX": "6",
+    "SEVEN": "7",
+    "EIGHT": "8",
+    "NINE": "9",
+    "TEN": "10",
+    "JACK": "J",
+    "QUEEN": "Q",
+    "KING": "K",
+    "ACE": "A",
+}
 
 
 class Rank(Enum):
@@ -23,8 +50,14 @@ class Rank(Enum):
     KING = 13
     ACE = 14
 
+    def __str__(self):
+        return RANK_DISPLAY_TEXT[self.name]
+
 
 class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
+
+    def __str__(self):
+        return f"{self.rank}{self.suit}"
